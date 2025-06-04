806092e56a7dcebf51d8f0102dc0f5daf5476145
Cory Richard <chipjohnson1211@gmail.com>
2025-06-03 21:33:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const clock = new THREE.Clock();
 // Holds all data related to the first 3D model
 const model1Data = {
     gltfModel: null, // The Three.js object for the model
-    modelUrl: 'https://raw.githubusercontent.com/RSOS-ops/CoryPill-2/main/HoodedCory_PlanarFace.glb', // URL to load the GLB/GLTF file
+    modelUrl: 'https://github.com/RSOS-ops/CoryPill-2/raw/main/HoodedCory_PlanarFace.glb', // URL to load the GLB/GLTF file
     initialScale: new THREE.Vector3(1, 1, 1), // Initial scale after normalization
     initialQuaternionDuringScaleUp: null,
     isRotationBoostActive: false, // Flag for rotation speed boost
